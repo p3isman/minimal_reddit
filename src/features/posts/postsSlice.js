7381a8ac870d4a8530943e5c9321daf7386bc67b
p3isman <pedro_eisman@hotmail.com>
2022-03-01 18:00:27 +0100

    Add postsSlice to store

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,103 @@
+import {
+  createAsyncThunk,
+  createSelector,
+  createSlice
+} from '@reduxjs/toolkit';
+import { RedditAPI } from '../../data/reddit_api';
+
+// Thunks
+export const getPostsBySubreddit = createAsyncThunk(
+  'posts/getPostsBySubreddit',
+  async subreddit => {
+    const posts = await RedditAPI.getPostsBySubreddit(subreddit);
+    return posts;
+  }
+);
+
+export const getPostComments = createAsyncThunk(
+  'posts/getPostComments',
+  async ({ index, postURL }) => {
+    const comments = await RedditAPI.getPostComments(postURL);
+    return { index, comments };
+  }
+);
+
+// Slice
+const initialState = {
+  posts: [],
+  error: false,
+  isLoading: false,
+  searchTerm: '',
+  selectedSubreddit: 'r/AskReddit'
+};
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    setSearchTerm: (state, action) => {
+      state.searchTerm = action.payload;
+    },
+    toggleShowComments: (state, action) => {
+      state.posts[action.payload].showComments =
+        !state.posts[action.payload].showComments;
+    },
+    selectSubreddit: (state, action) => {
+      state.selectedSubreddit = action.payload;
+    }
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(getPostsBySubreddit.pending, state => {
+        state.isLoading = true;
+        state.error = false;
+      })
+      .addCase(getPostsBySubreddit.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = false;
+        state.posts = action.payload;
+      })
+      .addCase(getPostsBySubreddit.rejected, state => {
+        state.isLoading = false;
+        state.error = true;
+      })
+      .addCase(getPostComments.pending, (state, action) => {
+        const { index } = action.payload;
+        state.posts[index].loadingComments = true;
+        state.posts[index].errorComments = false;
+      })
+      .addCase(getPostComments.fulfilled, (state, action) => {
+        const { index, comments } = action.payload;
+        state.posts[index].loadingComments = false;
+        state.posts[index].errorComments = false;
+        state.posts[index].comments = comments;
+      })
+      .addCase(getPostComments.rejected, (state, action) => {
+        const { index } = action.payload;
+        state.posts[index].loadingComments = true;
+        state.posts[index].errorComments = false;
+      });
+  }
+});
+
+export default postsSlice.reducer;
+
+// Selectors
+const selectSearchTerm = state => state.searchTerm;
+const selectPosts = state => state.posts;
+
+export const selectFilteredPosts = createSelector(
+  [selectSearchTerm, selectPosts],
+  (searchTerm, posts) => {
+    if (searchTerm !== '') {
+      return posts.filter(post =>
+        post.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+    return posts;
+  }
+);
+
+// Action creators
+export const { setSearchTerm, toggleShowComments, selectSubreddit } =
+  postsSlice;
