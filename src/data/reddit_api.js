ef74aa300a0bd64c8c4238778cdc492882f7bba2
p3isman <pedro_eisman@hotmail.com>
2022-03-01 17:58:58 +0100

    Add reddit api methods

---
+++ src/data/reddit_api.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'https://reddit.com/';
+
+export const RedditAPI = {
+  async getSubreddits() {
+    const response = await fetch(`${BASE_URL}subreddits.json`);
+    const json = await response.json();
+    return json.data.children.map(subreddit => subreddit.data);
+  },
+
+  async getPostsBySubreddit(subreddit) {
+    const response = await fetch(`${BASE_URL}${subreddit}.json`);
+    const json = await response.json();
+    return json.data.children.map(post => post.data);
+  },
+
+  async getPostComments(postURL) {
+    const response = await fetch(`${BASE_URL}${postURL}.json`);
+    const json = await response.json();
+    return json;
+  }
+};
