b1dd83b047f2b0f25388cdd1c8c4268fe2b5bc25
p3isman <pedro_eisman@hotmail.com>
2022-03-01 21:09:23 +0100

    Fix return value of getPostComments method

--- src/data/reddit_api.js
+++ src/data/reddit_api.js
@@ -16,6 +16,6 @@ export const RedditAPI = {
   async getPostComments(postURL) {
     const response = await fetch(`${BASE_URL}${postURL}.json`);
     const json = await response.json();
-    return json;
+    return json[1].data.children.map(comment => comment.data);
   }
 };
