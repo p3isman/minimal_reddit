1208b2ea89636644542f9462f1bab30481429d24
p3isman <pedro_eisman@hotmail.com>
2022-03-03 11:17:55 +0100

    Add app skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import './App.css';
+import Header from './Components/Header';
+import SideMenu from './Components/SideMenu';
+import './index.css';
 
 function App() {
-  return;
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main className='bg-zinc-100 w-screen h-screen'>
+        <SideMenu />
+      </main>
+    </>
+  );
 }
 
 export default App;
