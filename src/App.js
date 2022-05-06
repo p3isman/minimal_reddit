e8de2486b8f46d27bcb37a140ec9a47eb7b89a91
p3isman <pedro_eisman@hotmail.com>
2022-05-06 18:39:30 +0200

    Update style from main div

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Header from './Components/Header';
-import Posts from './Components/Posts';
-import SideMenu from './Components/SideMenu';
+import Header from './components/Header';
+import Posts from './components/Posts';
+import SideMenu from './components/SideMenu';
 import './index.css';
 
 function App() {
   return (
     <>
       <Header />
-      <main className='flex justify-center mt-20'>
+      <main className='flex justify-around mt-14'>
         <Posts />
         <SideMenu />
       </main>
