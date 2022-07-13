b2446b92107a5d4fd312635b13b96fa2af6318c6
p3isman <pedro_eisman@outlook.com>
2022-07-13 13:42:28 +0200

    Add animation to mobile menu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -5,6 +5,7 @@ import {
   selectSubredditsSlice
 } from '../features/subreddits/subredditsSlice';
 import Subreddit from './Subreddit';
+import './MobileMenu.css';
 
 const MobileMenu = ({ isVisible }) => {
   const subredditsSlice = useSelector(selectSubredditsSlice);
@@ -16,24 +17,25 @@ const MobileMenu = ({ isVisible }) => {
 
   return (
     <div
-      className={`fixed flex flex-wrap right-0 overflow-y-auto w-full h-1/3 bg-white mt-3 px-10 z-10 shadow-lg md:hidden ${
+      className={`mobile-menu fixed flex flex-wrap right-0 overflow-y-auto w-full h-1/3 bg-white mt-3 px-10 z-10 shadow-lg md:hidden ${
         !isVisible ? 'hidden' : ''
-      }`}>
+      }`}
+    >
       {error && <h3>Error loading subreddits</h3>}
       {isLoading && (
-        <div className='w-400 border-2 rounded-md p-9 mt-20'>
-          <div className='flex animate-pulse flex-col space-y-3'>
-            <div className='w-38 bg-gray-300 h-6 rounded-md'></div>
-            <div className='w-38 bg-gray-300 h-6 rounded-md'></div>
-            <div className='w-38 bg-gray-300 h-6 rounded-md'></div>
-            <div className='w-40 bg-gray-300 h-6 rounded-md'></div>
-            <div className='w-20 bg-gray-300 h-6 rounded-md'></div>
-            <div className='w-38 bg-gray-300 h-6 rounded-md'></div>
-            <div className='w-20 bg-gray-300 h-6 rounded-md'></div>
-            <div className='w-38 bg-gray-300 h-6 rounded-md'></div>
-            <div className='w-20 bg-gray-300 h-6 rounded-md'></div>
-            <div className='w-38 bg-gray-300 h-6 rounded-md'></div>
-            <div className='w-20 bg-gray-300 h-6 rounded-md'></div>
+        <div className="w-400 border-2 rounded-md p-9 mt-20">
+          <div className="flex animate-pulse flex-col space-y-3">
+            <div className="w-38 bg-gray-300 h-6 rounded-md"></div>
+            <div className="w-38 bg-gray-300 h-6 rounded-md"></div>
+            <div className="w-38 bg-gray-300 h-6 rounded-md"></div>
+            <div className="w-40 bg-gray-300 h-6 rounded-md"></div>
+            <div className="w-20 bg-gray-300 h-6 rounded-md"></div>
+            <div className="w-38 bg-gray-300 h-6 rounded-md"></div>
+            <div className="w-20 bg-gray-300 h-6 rounded-md"></div>
+            <div className="w-38 bg-gray-300 h-6 rounded-md"></div>
+            <div className="w-20 bg-gray-300 h-6 rounded-md"></div>
+            <div className="w-38 bg-gray-300 h-6 rounded-md"></div>
+            <div className="w-20 bg-gray-300 h-6 rounded-md"></div>
           </div>
         </div>
       )}
